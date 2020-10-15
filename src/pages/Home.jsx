de01caa4067f991deaa2ff5a414ef15d5c5c1a2b
Irungaray <LionelIrungaray@GMail.com>
2020-10-15 16:08:34 -0300

    Basic React Enviroment

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import './styles/Home.css'
+
+import Buttons from '../components/ButtonsHome'
+
+function Home() {
+  return (
+    <body>
+      <header>Sort Algorithms</header>
+
+      <main>
+        <h1>Select an Algorithm to see how it works:</h1>
+        <Buttons />
+      </main>
+
+      <footer>Irungaray</footer>
+    </body>
+  );
+}
+
+export default Home;
