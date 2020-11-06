3c95ac1b8ed5f2485f4cad723dfd13162ff4ef9c
Irungaray <LionelIrungaray@GMail.com>
2020-11-06 09:46:41 -0300

    Format

---
+++ src/components/algorithms/Bubble.js
@@ -0,0 +1,9 @@
+function Bubble() {
+    const numbers = [ 4, 6, 1, 3, 2, 7, 9, 8, 5]
+    for (let i = 0; i < numbers.length; i++) {
+        for (var j = 1; j < numbers.length; j++)
+        if (numbers, j)
+    }
+}
+
+Bubble()
